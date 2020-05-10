a941a8eb03a8889e7faf94423819c461da7b37bf
bo011300 <1498610052@qq.com>
2020-05-10 23:30:14 +0800

    Sync
    ii#   (use "git push" to publish your local commits)

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#0 SMP PREEMPT Sun May 10 23:28:02 CST 2020"
+#define LINUX_COMPILE_BY "clang-LTO"
+#define LINUX_COMPILE_HOST "MI 10 Pro"
+#define LINUX_COMPILER "Fiee-20200319 clang version 11.0.0 (git://github.com/llvm/llvm-project 00c1cd193497753b47d45c5f982e13b73215692a)"
---
+++ out/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.9.222-Fiee-Kernel-per"
